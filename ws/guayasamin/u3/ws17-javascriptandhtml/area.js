998c518cdd8ac58b55000e3b42aac5aa101010ed
Karla Guayasamin <kaguayasamin@espe.edu.ec>
2023-02-08 11:39:40 -0500

    ws17-htmlandjavascript

---
+++ ws/guayasamin/u3/ws17-javascriptandhtml/area.js
@@ -0,0 +1,8 @@
+var a=3
+console.log('a is apotema -->' + typeof a);
+var p=5
+console.log('p is perimeter  -->' + typeof p);
+
+//nombre incorrecto de variables 0/5
+
+console.log('The area of hexagon is '+ (p*a)/2);
