f278e1bd64da85e279708ad9f213de7d4f62658f
Anthony Quishpe <aequishpe2@espe.edu.ec>
2023-02-17 11:09:31 -0500

    ws19

---
+++ ws/quishpe/u3/ws19regEX/VehiclesRegistration/js/validate.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+let expressions={
+    plate: /^[^FfDd][a-zA-Z]{2}[0-9]{4}$/,
+    onlyLetters: /^[a-zA-Z\s-_\d]{1,30}$/,
+    model: /^[a-zA-Z\s.-\d]{1,10}$/
+}
+
+function validatePlate(){
+    let inp = document.getElementById('plate').value;
+    if(!(expressions.plate).test(inp)){
+        window.console.alert("Invalid Plate!!")
+        return false
+    } else{
+        return true;
+    }
+}
+/*
+function validateModel(){
+    let inp
+}
+
+function validateBrand(){
+    let expRegName=
+}*/
