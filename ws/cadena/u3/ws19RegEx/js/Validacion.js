90dc429760f6f9a0c6ab7e0c9e51afd95ccaf883
dacadena1 <diegocadena26@gamil.com>
2023-02-18 20:35:56 -0500

    'WSF'

---
+++ ws/cadena/u3/ws19RegEx/js/Validacion.js
@@ -0,0 +1,20 @@
+let expressions = {
+    plate: /^[^FfDd][a-zA-Z]{2}[0-9]{4}$/,
+    onlyLetters: /^[a-zA-Z\s-_\d]{1,30}$/,
+    model: /^[a-zA-Z\s.-\d]{1,10}$/
+}
+
+function vaidatePlate(){
+    let inp = document.getElementById('plate').value;
+    if(!(expressions.plate).test(inp)) {
+        window.alert("Invalid Plate");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validateModel(){
+    let inp=document.getElementById('model').value;
+    if(!(expressions.model).test)
+}
