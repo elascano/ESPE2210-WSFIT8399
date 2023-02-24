2035b131230dede5fed81f4a8812107cb67ebab5
Karla Guayasamin <kaguayasamin@espe.edu.ec>
2023-02-24 01:22:19 -0500

    changeimages

---
+++ hw/guayasamin/u3/HW09/js/changeproducts.js
@@ -0,0 +1,44 @@
+function changeImage1()
+{
+    document.getElementById("profesiongrande").src = "images/enfermero.jpg";
+}
+
+function changeImage2()
+{
+    document.getElementById("profesiongrande").src = "images/bombero.jpg";
+}
+
+function changeImage3()
+{
+    document.getElementById("profesiongrande").src = "images/fotografo.jpg";
+}
+
+function changeImage4()
+{
+    document.getElementById("profesiongrande").src = "images/policia.jpg";
+}
+
+function changeImage5()
+{
+    document.getElementById("profesiongrande").src = "images/doctor.jpg";
+}
+
+function changeImage6()
+{
+    document.getElementById("profesiongrande").src = "images/vaquero.jpg";
+}
+
+function changeImage7()
+{
+    document.getElementById("profesiongrande").src = "images/constructor.jpg";
+}
+
+function changeImage8()
+{
+    document.getElementById("profesiongrande").src = "images/piloto.jpg";
+}
+
+function changeImage9()
+{
+    document.getElementById("profesiongrande").src = "images/mecanico.jpg";
+}
