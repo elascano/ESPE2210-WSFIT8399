613c416a3db2a8631224ba787816a0ce5bb479cc
ddmoreno1 <ddmoreno1@espe.edu.ec>
2023-02-24 11:06:18 -0500

    The Sky

---
+++ ws/moreno/U3/WS20/js/changingImages.js
@@ -0,0 +1,21 @@
+//javascript tp load images
+function changeImage1()
+{
+    document.getElementById("bigPicture").src = "images/moto1.jpg";
+}
+
+function changeImage2()
+{
+    document.getElementById("bigPicture").src = "images/moto2.jpg";
+}
+
+function changeImage3()
+{
+    document.getElementById("bigPicture").src = "images/moto3.jpg";
+}
+
+function changeImage4()
+{
+    document.getElementById("bigPicture").src = "images/moto4.jpg";
+}
+
