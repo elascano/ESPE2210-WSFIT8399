ed678cc86663e2c84e280611a9d010d91bcd748d
lilichora <ldchora@espe.edu.ec>
2023-02-25 16:11:36 -0500

    workshop20

--- ws/chora/U3/WS20-Image/js/changingImages.js
+++ ws/chora/U3/WS20-Image/js/changingImages.js
@@ -1,17 +1,20 @@
 //javascript tp load images
 function changeImage1()
 {
-    document.getElementById("bigPicture").src = "images/alligator.jfif";
+    document.getElementById("bigPicture").src = "images/copito1.jpg";
 }
+
 function changeImage2()
 {
-    document.getElementById("bigPicture").src = "images/fatBoy.jfif";
+    document.getElementById("bigPicture").src = "images/copito2.jpg";
 }
+
 function changeImage3()
 {
-    document.getElementById("bigPicture").src = "images/gatorGo.jfif";
+    document.getElementById("bigPicture").src = "images/copito3.jpg";
 }
+
 function changeImage4()
 {
-    document.getElementById("bigPicture").src = "images/gatorRain.jfif";
+    document.getElementById("bigPicture").src = "images/copito4.jpg";
 }
