21dd34e3cc00b7457400d179e0d31047c7f16f9f
Cristian Idrobo <cfidrobo@espe.edu.ec>
2023-02-28 00:03:17 -0500

    hw09 update

--- hw/idrobo/u3/HW09 - Form/FormRegex/js/Regex.js
+++ hw/idrobo/u3/HW09 - Form/FormRegex/js/Regex.js
@@ -1,62 +1,53 @@
-function validarFormulario() {
-    // Obtener los valores de los campos de formulario
-    var name = document.getElementById("name").value;
-    var card = document.getElementById("Card").value;
-    var expiration = document.getElementById("Expiration").value;
-    var zip = document.getElementById("zip").value;
-  
-    // Validar los valores
-    var nameValido = validarNombre(name);
-    var cardValido = validarTarjeta(card);
-    var expirationValido = validarFecha(expiration);
-    var zipValido = validarCodigoPostal(zip);
-  
-    // Mostrar mensajes de error si es necesario
-    if (!nameValido) {
-      document.getElementById("nameError").innerHTML = "Por favor, ingrese un nombre válido.";
-    } else {
-      document.getElementById("nameError").innerHTML = "";
-    }
-  
-    if (!cardValido) {
-      document.getElementById("cardError").innerHTML = "Por favor, ingrese un número de tarjeta válido.";
-    } else {
-      document.getElementById("cardError").innerHTML = "";
-    }
-  
-    if (!expirationValido) {
-      document.getElementById("expirationError").innerHTML = "Por favor, ingrese una fecha de vencimiento válida.";
-    } else {
-      document.getElementById("expirationError").innerHTML = "";
-    }
-  
-    if (!zipValido) {
-      document.getElementById("zipError").innerHTML = "Por favor, ingrese un código postal válido.";
-    } else {
-      document.getElementById("zipError").innerHTML = "";
-    }
-  
-    // Devolver si el formulario es válido
-    return nameValido && cardValido && expirationValido && zipValido;
-  }
-  
-  function validarNombre(name) {
-    var regex = /^[A-Za-z]+$/;
-    return regex.test(name);
-  }
-  
-  function validarTarjeta(card) {
-    return card.length === 16;
-  }
-  
-  function validarFecha(expiration) {
-    var today = new Date();
-    var expirationDate = new Date(expiration);
-    return expirationDate > today;
-  }
-  
-  function validarCodigoPostal(zip) {
-    var regex = /^[0-9]{6}$/;
-    return regex.test(zip);
-  }
-  
+
+		function validarFormulario() {
+			// Obtener referencias a los campos del formulario
+			var name = document.getElementById("name");
+			var card = document.getElementById("Card");
+			var expiration = document.getElementById("Expiration");
+			var zip = document.getElementById("zip");
+
+			// Obtener referencias a los elementos de mensaje de error
+			var errorName = document.getElementById("error-name");
+			var errorCard = document.getElementById("error-card");
+			var errorExpiration = document.getElementById("error-expiration");
+			var errorZip = document.getElementById("error-zip");
+
+			// Bandera para indicar si el formulario es válido o no
+			var formularioValido = true;
+
+			// Validar el campo de nombre
+			if (!name.checkValidity()) {
+				errorName.innerHTML = "Please enter a valid name";
+				formularioValido = false;
+			} else {
+				errorName.innerHTML = "";
+			}
+
+			// Validar el campo de número de tarjeta
+			if (card.value.length < 16) {
+				errorCard.innerHTML = "Please enter a valid card number";
+				formularioValido = false;
+			} else
+      {
+        errorCard.innerHTML = "";
+        }
+        		// Validar el campo de fecha de vencimiento
+		if (expiration.value < "2023-02-20") {
+			errorExpiration.innerHTML = "Please enter a valid expiration date";
+			formularioValido = false;
+		} else {
+			errorExpiration.innerHTML = "";
+		}
+
+		// Validar el campo de código postal
+		if (zip.value.length < 6) {
+			errorZip.innerHTML = "Please enter a valid zip code";
+			formularioValido = false;
+		} else {
+			errorZip.innerHTML = "";
+		}
+
+		// Devolver si el formulario es válido o no
+		return formularioValido;
+	}
+
